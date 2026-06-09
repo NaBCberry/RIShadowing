f38fe7a77b58a93562210bbab04855c79654a992
NaBCberry <nabc_zhou@outlook.com>
2026-06-09 23:43:33 +0800

    chore: 删除临时文件

--- resize_icon.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-img = Image.open("D:/TraeProject/python/RIShadowing/RIShadowing.png").convert("RGBA")
-img.resize((64, 64), Image.LANCZOS).save("D:/TraeProject/python/RIShadowing/RIShadowing_icon.png")
-print("Created 64x64 PNG icon")
